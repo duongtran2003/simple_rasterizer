a6dcf80122d30eed3cad71391215b4b123bbd129
duongtran2003 <dt005250@email.com>
2026-04-17 14:21:13 +0700

    event poller

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,14 @@
 #include "SDL.h"
+#include "SDL_events.h"
 #include "SDL_pixels.h"
 #include "SDL_render.h"
-#include "SDL_timer.h"
 #include "SDL_video.h"
 #include <glm/glm.hpp>
-#include <iostream>
 
 const int WINDOW_W = 640;
 const int WINDOW_H = 480;
 
 int main() {
-  std::cout << "Hello world";
-
   SDL_Init(SDL_INIT_VIDEO);
   SDL_Window *window =
       SDL_CreateWindow("Rasterizer", SDL_WINDOWPOS_UNDEFINED,
@@ -20,10 +17,20 @@ int main() {
   SDL_Renderer *renderer =
       SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
 
-  SDL_Delay(3000);
+  bool isWindowRunning = true;
+  SDL_Event e;
+
+  while (isWindowRunning) {
+    while (SDL_PollEvent(&e) != 0) {
+      if (e.type == SDL_QUIT) {
+        isWindowRunning = false;
+      }
+      SDL_RenderClear(renderer);
+      SDL_RenderPresent(renderer);
+    }
+  }
+
   SDL_DestroyWindow(window);
   SDL_Quit();
 
