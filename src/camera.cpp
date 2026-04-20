c4b60b7bbc3fbb4291406792cb6940ebcaf50e4b
duongtran2003 <dt005250@gmail.com>
2026-04-20 17:03:20 +0700

    added camera boilerplate

---
+++ src/camera.cpp
@@ -0,0 +1,122 @@
+#include "camera.hpp"
+#include <algorithm>
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/vector_float3.hpp>
+#include <glm/fwd.hpp>
+#include <glm/geometric.hpp>
+#include <glm/trigonometric.hpp>
+
+namespace SimpleRenderer {
+Camera::Camera(glm::vec3 position, glm::vec3 direction, glm::vec3 worldUp,
+               float yaw, float pitch, float fov, float width, float height,
+               float near, float far) {
+  this->position = position;
+  this->direction = direction;
+  this->worldUp = worldUp;
+  this->yaw = yaw;
+  this->pitch = pitch;
+  this->fov = fov;
+  this->width = width;
+  this->height = height;
+  this->near = near;
+  this->far = far;
+
+  updateCameraVectors();
+}
+
+void Camera::updateCameraVectors() {
+  glm::vec3 calculatedDirection;
+  calculatedDirection.x =
+      glm::cos(glm::radians(yaw)) * glm::cos(glm::radians(pitch));
+  calculatedDirection.y = glm::sin(glm::radians(pitch));
+  calculatedDirection.z =
+      glm::sin(glm::radians(yaw)) * glm::cos(glm::radians(pitch));
+
+  direction = glm::normalize(calculatedDirection);
+  right = glm::normalize(glm::cross(direction, worldUp));
+  up = glm::normalize(glm::cross(right, direction));
+}
+
+glm::vec3 Camera::getPosition() const { return position; }
+glm::vec3 Camera::getDirection() const { return direction; }
+glm::vec3 Camera::getUp() const { return up; }
+glm::vec3 Camera::getRight() const { return right; }
+glm::vec3 Camera::getWorldUp() const { return worldUp; }
+
+float Camera::getYaw() const { return yaw; }
+float Camera::getPitch() const { return pitch; }
+float Camera::getFov() const { return fov; }
+float Camera::getWidth() const { return width; }
+float Camera::getHeight() const { return height; }
+float Camera::getNear() const { return near; }
+float Camera::getFar() const { return far; }
+
+glm::mat4 Camera::getViewMatrix() const {
+  return glm::lookAt(position, direction, up);
+}
+
+glm::mat4 Camera::getProjectionMatrix() const {
+  return glm::perspective(fov, width / height, near, far);
+}
+
+void Camera::setPosition(glm::vec3 position) { this->position = position; }
+void Camera::getDirection(glm::vec3 direction) {
+  this->direction = direction;
+  updateCameraVectors();
+}
+void Camera::setWorldUp(glm::vec3 worldUp) {
+  this->worldUp = worldUp;
+  updateCameraVectors();
+}
+void Camera::setYaw(float yaw) {
+  this->yaw = yaw;
+  updateCameraVectors();
+}
+void Camera::setPitch(float pitch) {
+  this->pitch = pitch;
+  updateCameraVectors();
+}
+void Camera::setFov(float fov) { this->fov = fov; }
+void Camera::setWidth(float width) { this->width = width; }
+void Camera::setHeight(float height) { this->height = height; }
+void Camera::setNear(float near) { this->near = near; }
+void Camera::setFar(float far) { this->far = far; }
+
+void Camera::processCameraMovement(Movement movement, double deltaTime) {
+  float velocity = 100.0f * deltaTime;
+  switch (movement) {
+  case Forward:
+    position += direction * velocity;
+    break;
+  case Backward:
+    position -= direction * velocity;
+    break;
+  case Left:
+    position -= right * velocity;
+    break;
+  case Right:
+    position += right * velocity;
+    break;
+  case Up:
+    position += up * velocity;
+    break;
+  case Down:
+    position -= up * velocity;
+    break;
+  default:
+    break;
+  }
+}
+
+void Camera::processCameraAngle(glm::vec2 positionOffset) {
+  glm::vec2 sensitivity = glm::vec2(10.0f, 6.0f);
+  glm::vec2 offset = glm::vec2(positionOffset.x * sensitivity.x,
+                               -positionOffset.y * sensitivity.y);
+
+  yaw += offset.x;
+  pitch += offset.y;
+
+  updateCameraVectors();
+}
+} // namespace SimpleRenderer
---
+++ src/camera.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <glm/fwd.hpp>
+#include <glm/glm.hpp>
+
+namespace SimpleRenderer {
+class Camera {
+public:
+  enum Movement { Forward, Backward, Left, Right, Up, Down };
+
+  inline static constexpr glm::vec3 POSITION = glm::vec3(0.0f, 0.0f, 0.0f);
+  inline static constexpr glm::vec3 DIRECTION = glm::vec3(0.0f, 0.0f, 1.0f);
+  inline static constexpr glm::vec3 WORLD_UP = glm::vec3(0.0f, 1.0f, 0.0f);
+
+  inline static constexpr float YAW = -90.0f;
+  inline static constexpr float PITCH = 0.0f;
+  inline static constexpr float FOV = 90.0f;
+
+  inline static constexpr int WIDTH = 640;
+  inline static constexpr int HEIGHT = 480;
+  inline static constexpr float NEAR = 1.0f;
+  inline static constexpr float FAR = 100.0f;
+
+  Camera(glm::vec3 position = POSITION, glm::vec3 direction = DIRECTION,
+         glm::vec3 worldUp = WORLD_UP,
+
+         float yaw = YAW, float pitch = PITCH, float fov = FOV,
+
+         float width = WIDTH, float height = HEIGHT, float near = NEAR,
+         float far = FAR);
+
+  glm::vec3 getPosition() const;
+  glm::vec3 getDirection() const;
+  glm::vec3 getUp() const;
+  glm::vec3 getRight() const;
+  glm::vec3 getWorldUp() const;
+
+  float getYaw() const;
+  float getPitch() const;
+  float getFov() const;
+
+  float getWidth() const;
+  float getHeight() const;
+  float getNear() const;
+  float getFar() const;
+
+  glm::mat4 getViewMatrix() const;
+  glm::mat4 getProjectionMatrix() const;
+
+  void setPosition(glm::vec3 position);
+  void getDirection(glm::vec3 direction);
+  void setWorldUp(glm::vec3 worldUp);
+
+  void setYaw(float yaw);
+  void setPitch(float pitch);
+  void setFov(float fov);
+
+  void setWidth(float width);
+  void setHeight(float height);
+  void setNear(float near);
+  void setFar(float far);
+
+  void processCameraMovement(Movement movement, double deltaTime);
+  void processCameraAngle(glm::vec2 positionOffset);
+
+private:
+  glm::vec3 position;
+  glm::vec3 direction;
+  glm::vec3 up;
+  glm::vec3 right;
+  glm::vec3 worldUp;
+
+  float yaw;
+  float pitch;
+  float fov;
+
+  float width;
+  float height;
+  float near;
+  float far;
+
+  void updateCameraVectors();
+};
+} // namespace SimpleRenderer
